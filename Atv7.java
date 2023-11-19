ff6931441ba0b91f793fa85c2271a41845dfa859
Adriana Vilar <adriana.correa@outlook.com>
2023-11-19 19:12:56 -0300

    correcao das validacoes

--- Atv7.java
+++ Atv7.java
@@ -1,4 +1,4 @@
-public class Atv7 {
+public class Atv8 {
     public static void main(String[] args) {
 
         boolean temNumAlgarismos;
@@ -9,44 +9,13 @@ public class Atv7 {
         boolean menosDeTrezAlgarismosConsecutivos;
         boolean menosDeDozeAlgarismosUsados;
         boolean peloMenosOitoAlgarismoUsados;
-        int contador = 0;
-        int somador = 7;
+        int somador = 1;
 
         int[] array = new int[54];
         int[] arrayCompleto = new int[109];
 
-        for (int i = 0; i < array.length; i++) {
-            if (i % 2 == 0 && i != 0) {
-                somador++;
-                if (somador == 10) {
-                    somador = 0;
-                }
-            }
-            array[i] = somador;
-        }
-
-        printArray(array);
-
-        System.out.println(verificaSeTemTresConsecutivosIguais(array));
-
-        for (int i = 0, j = arrayCompleto.length - 1; i < array.length; i++, j--) {
-            arrayCompleto[i] = array[i];
-            arrayCompleto[j] = array[i];
-        }
-
-        arrayCompleto[54] = 0;
-        // arrayCompleto[0] = 4;
-        // arrayCompleto[108] = 4;
-        printArray(arrayCompleto);
-        System.out.println(verificaCapicua(arrayCompleto));
-        System.out.println(verificaMultiploOnze(arrayCompleto));
-        System.out
-                .println(verificaMultiploTreze(new int[] { 1, 2, 3, 4, 5, 6, 7, 9, 8, 1, 3, 5, 7, 8, 5, 6, 8, 1, 6 }));
-        System.out.println(verificaMultiploSete(new int[] { 1, 2, 5, 7, 9, 6, 4, 3, 2, 7, 1, 3, 5, 6 }));
-
         do {
-            if (true)
-                break;
+
             temNumAlgarismos = false;
             ehCapicua = false;
             multiploSete = false;
@@ -56,65 +25,71 @@ public class Atv7 {
             menosDeDozeAlgarismosUsados = false;
             peloMenosOitoAlgarismoUsados = false;
 
-            do {
-                incrementaArray(array);
-                printArray(array);
+            for (int i = 0; i < array.length; i++) {
+                if (i % 2 == 0 && i != 0) {
+                    somador++;
+                    if (somador == 10) {
+                        somador = 0;
+                    }
+                }
+                array[i] = somador;
+            }
 
-            } while (verificaSeTemTresConsecutivosIguais(array));
+            printArray(array);
 
+            System.out.println("3 consecutivos: " + verificaSeTemTresConsecutivosIguais(array));
+
+            incrementaArray(array);
+            printArray(array);
+
+            for (int i = 0, j = arrayCompleto.length - 1; i < array.length; i++, j--) {
+                arrayCompleto[i] = array[i];
+                arrayCompleto[j] = array[i];
+            }
+
+            arrayCompleto[54] = 0;
+            printArray(arrayCompleto);
             /*
-             * for (int i = 0; i < array.length; i++) {
-             * arrayCompleto[i] = array[i];
-             * }
-             * 
-             * arrayCompleto[55] = array[53];
-             * 
-             * int cont = 4;
-             * for (int i = 56; i < array.length; i++) {
-             * arrayCompleto[i] = array[i - cont];
-             * cont += 2;
-             * }
-             * 
-             * temNumAlgarismos = verificaQtdAlgarismos(array);
-             * ehCapicua = verificaCapicua(array);
-             * if (!ehCapicua) {
-             * continue;
-             * }
-             * 
-             * multiploSete = verificaMultiploSete(array);
-             * if (!multiploSete) {
-             * continue;
-             * }
-             * 
-             * multiploOnze = verificaMultiploOnze(array);
-             * if (!multiploOnze) {
-             * continue;
-             * }
-             * 
-             * multiploTreze = verificaMultiploTreze(array);
-             * 
-             * menosDeTrezAlgarismosConsecutivos =
-             * !verificaSeTemTresConsecutivosIguais(array);
-             * menosDeDozeAlgarismosUsados = !verificaSeTemMaisDeDozeVezes(array);
-             * peloMenosOitoAlgarismoUsados = verificaSeTemPeloMenosOito(array);
-             * 
-             * System.out.println("Tem 109 algarismos = " + verificaQtdAlgarismos(array));
-             * System.out.println("Eh capicua = " + verificaCapicua(array));
-             * 
-             * System.out.println("Eh multiplo de 7 = " + verificaMultiploSete(array));
-             * System.out.println("Eh multiplo de 13 = " + verificaMultiploTreze(array));
-             * System.out.println("Eh multiplo de 11 = " + verificaMultiploOnze(array));
-             * 
-             * System.out.println("Tem mais de 12 vezes o mesmo algarismo = " +
-             * verificaSeTemMaisDeDozeVezes(array));
-             * System.out.println("Tem pelo menos 8 vezes o mesmo algarismo = " +
-             * verificaSeTemPeloMenosOito(array));
-             * System.err.println("Tem 3 consecutivos iguais = " +
-             * verificaSeTemTresConsecutivosIguais(array));
+             * System.out.println("Capicua: " + verificaCapicua(arrayCompleto));
+             * System.out.println("7: " + verificaMultiploSete(arrayCompleto));
+             * System.out.println("11: " + verificaMultiploOnze(arrayCompleto));
+             * System.out.println("13: " + verificaMultiploTreze(arrayCompleto));
              */
 
-        } while (!temNumAlgarismos ||
-                !multiploSete ||
+            temNumAlgarismos = verificaQtdAlgarismos(arrayCompleto);
+            ehCapicua = verificaCapicua(arrayCompleto);
+            if (!ehCapicua) {
+                continue;
+            }
+
+            multiploSete = verificaMultiploSete(arrayCompleto);
+            if (!multiploSete) {
+                continue;
+            }
+
+            multiploOnze = verificaMultiploOnze(arrayCompleto);
+            if (!multiploOnze) {
+                continue;
+            }
+
+            multiploTreze = verificaMultiploTreze(arrayCompleto);
+
+            menosDeTrezAlgarismosConsecutivos = !verificaSeTemTresConsecutivosIguais(arrayCompleto);
+            menosDeDozeAlgarismosUsados = !verificaSeTemMaisDeDozeVezes(arrayCompleto);
+            peloMenosOitoAlgarismoUsados = verificaSeTemPeloMenosOito(arrayCompleto);
+
+            System.out.println("Tem 109 algarismos = " + verificaQtdAlgarismos(arrayCompleto));
+            System.out.println("Eh capicua = " + verificaCapicua(arrayCompleto));
+
+            System.out.println("Eh multiplo de 7 = " + verificaMultiploSete(arrayCompleto));
+            System.out.println("Eh multiplo de 13 = " + verificaMultiploTreze(arrayCompleto));
+            System.out.println("Eh multiplo de 11 = " + verificaMultiploOnze(arrayCompleto));
+
+            System.out.println("Tem mais de 12 vezes = " + verificaSeTemMaisDeDozeVezes(arrayCompleto));
+            System.out.println("Tem 8 vezes o mesmo algarismo = " + verificaSeTemPeloMenosOito(arrayCompleto));
+            System.out.println("Tem 3 consecutivos = " + verificaSeTemTresConsecutivosIguais(arrayCompleto));
+
+        } while (!multiploSete ||
                 !multiploOnze ||
                 !multiploTreze ||
                 !menosDeTrezAlgarismosConsecutivos ||
@@ -174,28 +149,21 @@ public class Atv7 {
     }
 
     public static boolean verificaMultiploSete(int[] array) {
-        int[] arrayCompleto = new int[array.length + array.length % 3];
+        int qtdZeros = 3 - array.length % 3;
+        int[] arrayCompleto = new int[array.length + qtdZeros];
         int[][] grupoDeTres = new int[arrayCompleto.length / 3][3];
         int[] arraySomatorio = new int[arrayCompleto.length / 3];
         int cont;
         int somatorio = 0;
 
-        for (int i = 2; i < arrayCompleto.length; i++) {
-            arrayCompleto[i] = array[i - 2];
+        for (int i = qtdZeros, j = 0; i < arrayCompleto.length; i++, j++) {
+            arrayCompleto[i] = array[j];
         }
-
-        /*
-         * for (int i = 0; i < arrayCompleto.length; i++) {
-         * arrayCompleto[i] = array[i];
-         * }
-         */
 
         grupoDeTres = divideEmGruposDeTres(arrayCompleto);
 
         for (int i = 0; i < arraySomatorio.length; i++) {
             arraySomatorio[i] = grupoDeTres[i][2] + grupoDeTres[i][1] * 3 + grupoDeTres[i][0] * 2;
-            System.out.println("ARRAY SOMATORIO 7[" + i + "] = " + arraySomatorio[i]);
-
         }
 
         cont = arraySomatorio.length;
@@ -225,31 +193,21 @@ public class Atv7 {
     }
 
     public static boolean verificaMultiploTreze(int[] array) {
-        int[] arrayCompleto = new int[array.length + array.length % 3];
+        int qtdZeros = 3 - array.length % 3;
+        int[] arrayCompleto = new int[array.length + qtdZeros];
         int[][] grupoDeTres = new int[arrayCompleto.length / 3][3];
         int[] arraySomatorio = new int[arrayCompleto.length / 3];
         int cont;
         int somatorio = 0;
 
-        for (int i = 2; i < arrayCompleto.length; i++) {
-            arrayCompleto[i] = array[i - 2];
+        for (int i = qtdZeros, j = 0; i < arrayCompleto.length; i++, j++) {
+            arrayCompleto[i] = array[j];
         }
-
-        /*
-         * for (int i = 0; i < arrayCompleto.length; i++) {
-         * arrayCompleto[i] = array[i];
-         * }
-         */
 
         grupoDeTres = divideEmGruposDeTres(arrayCompleto);
 
         for (int i = 0; i < arraySomatorio.length; i++) {
             arraySomatorio[i] = grupoDeTres[i][2] * (-1) + grupoDeTres[i][1] * 3 + grupoDeTres[i][0] * 4;
-            System.out.println("GRUPO[" + i + "][2] = " + grupoDeTres[i][2]);
-            System.out.println("GRUPO[" + i + "][1] = " + grupoDeTres[i][1]);
-            System.out.println("GRUPO[" + i + "][0] = " + grupoDeTres[i][0]);
-
-            System.out.println("ARRAY SOMATORIO[" + i + "] = " + arraySomatorio[i]);
         }
 
         cont = arraySomatorio.length;
@@ -261,17 +219,17 @@ public class Atv7 {
 
             if (ehPar) {
                 if (i % 2 == 0) {
-                    somatorio += arraySomatorio[cont - i];
-                } else {
                     somatorio -= arraySomatorio[cont - i];
+                } else {
+                    somatorio += arraySomatorio[cont - i];
                 }
             }
 
             if (!ehPar) {
                 if (i % 2 == 0) {
-                    somatorio -= arraySomatorio[cont - i];
-                } else {
                     somatorio += arraySomatorio[cont - i];
+                } else {
+                    somatorio -= arraySomatorio[cont - i];
                 }
             }
         }
@@ -286,12 +244,6 @@ public class Atv7 {
         for (int i = 2; i < arrayCompleto.length; i++) {
             arrayCompleto[i] = array[i - 2];
         }
-
-        /*
-         * for (int i = 0; i < arrayCompleto.length; i++) {
-         * arrayCompleto[i] = array[i];
-         * }
-         */
 
         cont = arrayCompleto.length;
         boolean ehPar = true;
@@ -333,6 +285,7 @@ public class Atv7 {
                 break;
             }
         }
+        System.err.println("QTD de cada algorismo: ");
         printArray(qtdCadaAlgarismo);
         return temMaisDeDozeVezes;
     }
@@ -356,20 +309,10 @@ public class Atv7 {
     }
 
     public static boolean verificaSeTemTresConsecutivosIguais(int[] array) {
-        // boolean temTresConsecutivos = false;
-
         for (int j = 0; j < array.length - 3; j++) {
             if (array[j] == array[j + 1] && array[j] == array[j + 2]) {
                 return true;
-
-                // if (array[j + 1] == array[j + 2]) {
-                // temTresConsecutivos = true;
-                // } else {
-                // temTresConsecutivos = false;
-                // }
-            } // else {
-              // temTresConsecutivos = false;
-              // }
+            }
         }
 
         return false;
